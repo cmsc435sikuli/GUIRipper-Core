f2fc3cfacbec2883c1baa0abf2ab42d1094c012c
jsternberg <jonathansternberg@gmail.com>
2011-02-07 23:36:03 -0500

    Moved boilerplate code to the RipperMain implementation.

--- src/edu/umd/cs/guitar/ripper/GRipperConfiguration.java
+++ src/edu/umd/cs/guitar/ripper/GRipperConfiguration.java
@@ -2,7 +2,18 @@ package edu.umd.cs.guitar.ripper;
 
 import org.kohsuke.args4j.Option;
 
+import edu.umd.cs.guitar.util.Util;
+
 public class GRipperConfiguration {
     @Option(name = "--gui-file", usage = "destination GUI file path", aliases = "-g")
     public static String GUI_FILE = "GUITAR-Default.GUI";
+
+    @Option(name = "--log-file", usage = "log file name ", aliases = "-l")
+    public static String LOG_FILE = Util.getTimeStamp() + ".log";
+
+    @Option(name = "--open-win-file", usage = "log file name", aliases = "-ow")
+    public static String LOG_WIDGET_FILE = "log_widget.xml";
+
+    @Option(name = "--config-file", usage = "Configuration file for the ripper defining terminal and ignored components/windows", aliases = "-cf")
+    public static String CONFIG_FILE = "configuration.xml";
 }
--- src/edu/umd/cs/guitar/ripper/RipperMain.java
+++ src/edu/umd/cs/guitar/ripper/RipperMain.java
@@ -1,9 +1,35 @@
 package edu.umd.cs.guitar.ripper;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.TimeZone;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 
-public class RipperMain {
+import edu.umd.cs.guitar.model.GIDGenerator;
+import edu.umd.cs.guitar.model.IO;
+import edu.umd.cs.guitar.model.data.AttributesType;
+import edu.umd.cs.guitar.model.data.ComponentListType;
+import edu.umd.cs.guitar.model.data.ComponentType;
+import edu.umd.cs.guitar.model.data.Configuration;
+import edu.umd.cs.guitar.model.data.FullComponentType;
+import edu.umd.cs.guitar.model.data.GUIStructure;
+import edu.umd.cs.guitar.model.data.LogWidget;
+import edu.umd.cs.guitar.model.data.ObjectFactory;
+import edu.umd.cs.guitar.model.wrapper.AttributesTypeWrapper;
+import edu.umd.cs.guitar.model.wrapper.ComponentTypeWrapper;
+import edu.umd.cs.guitar.model.GUITARConstants;
+import edu.umd.cs.guitar.util.GUITARLog;
+
+/** Base template for ripper main function. This takes care of a lot
+    of the boilerplate code such as setting up the logger, the ripper
+    monitor, the id generator, and the widget configuration file.
+ */
+public abstract class RipperMain {
     private GRipperConfiguration config;
 
     public RipperMain(GRipperConfiguration config) {
@@ -13,7 +39,88 @@ public class RipperMain {
     private Ripper ripper;
 
     public void execute() {
-        System.out.println("executing the ripper main");
-        System.out.println(config.GUI_FILE);
+        System.setProperty("file.name", config.LOG_FILE);
+        PropertyConfigurator.configure(GUITARConstants.LOG4J_PROPERTIES_FILE);
+
+        GUITARLog.log = Logger.getLogger(this.getClass().getSimpleName());
+        long nStartTime = System.currentTimeMillis();
+
+        ripper = new Ripper(GUITARLog.log);
+
+        try {
+            setupEnv();
+        } catch (Exception e) {
+            GUITARLog.log.error("RipperMain: ", e);
+        }
+
+        GUIStructure dGUIStructure = ripper.getResult();
+        IO.writeObjToFile(dGUIStructure, config.GUI_FILE);
+
+        GUITARLog.log.info("-----------------------------");
+        GUITARLog.log.info("OUTPUT SUMARY: ");
+        GUITARLog.log.info("Number of Windows: "
+                + dGUIStructure.getGUI().size());
+        GUITARLog.log.info("GUI file:" + config.GUI_FILE);
+        GUITARLog.log.info("Open Component file:" + config.LOG_WIDGET_FILE);
+        ComponentListType lOpenWins = ripper.getlOpenWindowComps();
+        ComponentListType lCloseWins = ripper.getlCloseWindowComp();
+        ObjectFactory factory = new ObjectFactory();
+
+        LogWidget logWidget = factory.createLogWidget();
+        logWidget.setOpenWindow(lOpenWins);
+        logWidget.setCloseWindow(lCloseWins);
+
+        IO.writeObjToFile(logWidget, config.LOG_WIDGET_FILE);
+
+        // ------------------
+        // Elapsed time:
+        long nEndTime = System.currentTimeMillis();
+        long nDuration = nEndTime - nStartTime;
+        DateFormat df = new SimpleDateFormat("HH : mm : ss: SS");
+        df.setTimeZone(TimeZone.getTimeZone("GMT"));
+        GUITARLog.log.info("Ripping Elapsed: " + df.format(nDuration));
+        GUITARLog.log.info("Log file: " + config.LOG_FILE);
     }
+
+    public void setupEnv() {
+        // Load configuration file from disk
+        Configuration conf = (Configuration) IO.readObjFromFile(
+            config.CONFIG_FILE, Configuration.class);
+
+        if (conf != null) {
+            // Load the terminal/ignored windows from the configuration
+            loadWidgetConfiguration(
+                conf.getTerminalComponents().getFullComponent(),
+                GUITARConstants.terminalWidgetSignature);
+            loadWidgetConfiguration(
+                conf.getIgnoredComponents().getFullComponent(),
+                GUITARConstants.ignoredWidgetSignature);
+        }
+
+        GRipperMonitor monitor = createMonitor(config);
+        ripper.setMonitor(monitor);
+
+        GIDGenerator idGenerator = getIdGenerator();
+        ripper.setIDGenerator(idGenerator);
+    }
+
+    private void loadWidgetConfiguration(
+        List<FullComponentType> widgetList,
+        List<AttributesTypeWrapper> widgetSignatures)
+    {
+        for (FullComponentType widget : widgetList) {
+            ComponentType component = widget.getWindow();
+            if (component == null) {
+                component = widget.getComponent();
+            }
+            AttributesType attributes = component.getAttributes();
+            widgetSignatures.add(
+                new AttributesTypeWrapper(component.getAttributes()));
+        }
+    }
+
+    protected abstract GRipperMonitor
+    createMonitor(GRipperConfiguration config);
+
+    protected abstract GIDGenerator getIdGenerator();
 }
