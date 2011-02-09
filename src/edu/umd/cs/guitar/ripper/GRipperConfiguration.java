6079a2afe9dc7c76ad6eb0f3cb3bf50072df7542
jsternberg <jonathansternberg@gmail.com>
2011-02-08 22:55:42 -0500

    Added the remaining command line options and a help option.

--- src/edu/umd/cs/guitar/ripper/GRipperConfiguration.java
+++ src/edu/umd/cs/guitar/ripper/GRipperConfiguration.java
@@ -5,6 +5,7 @@ import org.apache.commons.cli.Options;
 import org.apache.commons.cli.OptionBuilder;
 import org.apache.commons.cli.PosixParser;
 import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.HelpFormatter;
 
 import edu.umd.cs.guitar.util.Util;
 
@@ -18,31 +19,35 @@ public class GRipperConfiguration {
 
     public GRipperConfiguration() {
         opts = new Options();
-        opts.addOption( OptionBuilder.withLongOpt("gui-file")
-                        .withDescription("destination GUI file path")
-                        .hasArg()
-                        .withArgName("GUI_FILE")
-                        .create() );
+        opts.addOption( "g", "gui-file", true, "destination GUI file path" );
+        opts.addOption( "l", "log-file", true, "log file name" );
+        opts.addOption( null, "open-win-file", true, "widget log file" );
+        opts.addOption( "c", "config-file", true, "configuration file for the ripper defining terminal and ignored components/windows" );
+        opts.addOption( "d", "delay", true, "delay between actions" );
+        opts.addOption( "h", "help", false, "Print this message" );
     }
 
     public void parseArguments(String[] args) throws ParseException {
         PosixParser parser = new PosixParser();
         cmd = parser.parse( opts, args );
+
+        if ( cmd.hasOption("help") ) {
+            // Print help and exit with non-zero status code
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp( "RipperMain", opts );
+            System.exit(1);
+        }
         
         GUI_FILE = cmd.getOptionValue("gui-file", "GUITAR-Default.GUI");
-        //LOG_FILE = cmd.getOptionValue("log-file", Util.getTimeStamp()+".log");
+        LOG_FILE = cmd.getOptionValue("log-file", Util.getTimeStamp()+".log");
+        LOG_WIDGET_FILE = cmd.getOptionValue("open-win-file", "log_widget.xml");
+        CONFIG_FILE = cmd.getOptionValue("config-file", "configuration.xml");
+        DELAY = Integer.parseInt(cmd.getOptionValue("delay", "5000"));
     }
 
     public static String GUI_FILE;
-
-    // @Option(name = "--log-file", usage = "log file name ", aliases = "-l")
-    public static String LOG_FILE = Util.getTimeStamp() + ".log";
-
-    // @Option(name = "--open-win-file", usage = "log file name", aliases = "-ow")
-    public static String LOG_WIDGET_FILE = "log_widget.xml";
-
-    // @Option(name = "--config-file", usage = "Configuration file for the ripper defining terminal and ignored components/windows", aliases = "-cf")
-    public static String CONFIG_FILE = "configuration.xml";
-
+    public static String LOG_FILE;
+    public static String LOG_WIDGET_FILE;
+    public static String CONFIG_FILE;
     public static int DELAY = 5000;
 }
